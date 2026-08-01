9e540775cabe209c00ff65c8e32dcec81c5cf813
Glec6 <149967036+Glec6@users.noreply.github.com>
2026-08-01 14:51:28 -0500

    Update ps4.cmake

--- cmake/ps4.cmake
+++ cmake/ps4.cmake
@@ -1,18 +1,31 @@
 set(CMAKE_SYSTEM_NAME FreeBSD)
 set(CMAKE_SYSTEM_PROCESSOR x86_64)
 
-if(NOT DEFINED ENV{OO_PS4_TOOLCHAIN})
-    set(ENV{OO_PS4_TOOLCHAIN} "/opt/openorbis")
+# Define OpenOrbis root
+if(NOT DEFINED ENV{OPENORBIS})
+    set(ENV{OPENORBIS} "/opt/pacbrew/ps4/openorbis")
 endif()
 
-set(PS4_SDK $ENV{OO_PS4_TOOLCHAIN})
+set(PS4_SDK $ENV{OPENORBIS})
 
-set(CMAKE_C_COMPILER "${PS4_SDK}/bin/orbis-clang")
-set(CMAKE_CXX_COMPILER "${PS4_SDK}/bin/orbis-clang++")
-set(CMAKE_AR "${PS4_SDK}/bin/orbis-ar" CACHE FILEPATH "Archiver")
-set(CMAKE_LINKER "${PS4_SDK}/bin/orbis-ld" CACHE FILEPATH "Linker")
+# Check if orbis-clang wrapper exists, otherwise fall back to system clang
+if(EXISTS "${PS4_SDK}/bin/orbis-clang")
+    set(CMAKE_C_COMPILER "${PS4_SDK}/bin/orbis-clang")
+    set(CMAKE_CXX_COMPILER "${PS4_SDK}/bin/orbis-clang++")
+elseif(EXISTS "${PS4_SDK}/llvm/bin/clang")
+    set(CMAKE_C_COMPILER "${PS4_SDK}/llvm/bin/clang")
+    set(CMAKE_CXX_COMPILER "${PS4_SDK}/llvm/bin/clang++")
+else()
+    # Fallback to standard clang compiler on Ubuntu runner with target triple
+    set(CMAKE_C_COMPILER "clang")
+    set(CMAKE_CXX_COMPILER "clang++")
+    set(CMAKE_C_FLAGS "--target=x86_64-scei-ps4 -sysroot=${PS4_SDK}")
+    set(CMAKE_CXX_FLAGS "--target=x86_64-scei-ps4 -sysroot=${PS4_SDK}")
+endif()
 
-include_directories(${PS4_SDK}/include)
-link_directories(${PS4_SDK}/lib)
+set(CMAKE_ASM_COMPILER ${CMAKE_C_COMPILER})
+
+include_directories(${PS4_SDK}/include ${PS4_SDK}/target/include)
+link_directories(${PS4_SDK}/lib ${PS4_SDK}/target/lib)
 
 add_definitions(-D__PS4__ -DORBIS -D_GLES2)
